fbf7152737eeed4bef360616608b7c0a1e36c1a3
alex <2930807240>
2019-01-10 10:21:58 +0800

    init

--- ArticleSpider/pipelines.py
+++ ArticleSpider/pipelines.py
@@ -10,60 +10,100 @@ from scrapy.pipelines.images import ImagesPipeline
 import codecs
 from scrapy.exporters import JsonItemExporter
 import MySQLdb
+from twisted.enterprise import adbapi
 
 
 class ArticlespiderPipeline(object):
-	def process_item(self, item, spider):
-		return item
+    def process_item(self, item, spider):
+        return item
 
 
 class JsonWithEncodingPipeline(object):
-	# 自定义的json文件导出
-	def __init__(self):
-		self.file = codecs.open('article.json', 'w', encoding='utf-8')
-	
-	def process_item(self, item, spider):
-		lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
-		self.file.write(lines)
-		return item
-	
-	def spider_closed(self, spider):
-		self.file.close()
+    # 自定义的json文件导出
+    def __init__(self):
+        self.file = codecs.open('article.json', 'w', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
 
 
 class MysqlPipeline(object):
-	def __init__(self):
-		self.conn = MySQLdb.connect('localhost', 'root', 'qwerty123', 'article_spider', charset='utf8', use_unicode=True)
-		self.cursor = self.conn.cursor()
-	
-	def process_item(self, item, spider):
-		insert_sql = """
+    def __init__(self):
+        self.conn = MySQLdb.connect('localhost', 'root', 'qwerty123', 'article_spider', charset='utf8',
+                                    use_unicode=True)
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        insert_sql = """
 			insert into jobbole_article(url_object_id, title, url, create_date, fav_nums, praise_nums, comment_nums, front_image_url, content, tags, front_image_path)
 			values (%s, %s, %s, %s, %s, %s, %s, %s , %s, %s, %s)
 		"""
-		self.cursor.execute(insert_sql, (item['url_object_id'], item['title'], item['url'], item['create_date'], int(item['fav_nums']), int(item['praise_nums']), int(item['comment_nums']), item['front_image_url'], item['content'], item['tags'], item['front_image_path']))
-		self.conn.commit()
+        self.cursor.execute(insert_sql, (
+            item['url_object_id'], item['title'], item['url'], item['create_date'], int(item['fav_nums']),
+            int(item['praise_nums']), int(item['comment_nums']), item['front_image_url'], item['content'], item['tags'],
+            item['front_image_path']))
+        self.conn.commit()
+
+
+class MysqlTwistedPipline(object):
+    def __init__(self, dbpool):
+        self.dbpool = dbpool
+
+    @classmethod
+    def from_settings(cls, settings):
+        dbparms = dict(
+            host=settings["MYSQL_HOST"],
+            db=settings["MYSQL_DBNAME"],
+            user=settings["MYSQL_USER"],
+            passwd=settings["MYSQL_PASSWORD"],
+            charset='utf8',
+            cursorclass=MySQLdb.cursors.DictCursor,
+            use_unicode=True,
+        )
+        dbpool = adbapi.ConnectionPool("MySQLdb", **dbparms)
+
+        return cls(dbpool)
+
+    def process_item(self, item, spider):
+        # 使用twisted将mysql插入变成异步执行
+        query = self.dbpool.runInteraction(self.do_insert, item)
+        query.addErrback(self.handle_error, item, spider)  # 处理异常
+
+    def handle_error(self, failure, item, spider):
+        # 处理异步插入的异常
+        print(failure)
+
+    def do_insert(self, cursor, item):
+        # 执行具体的插入
+        # 根据不同的item 构建不同的sql语句并插入到mysql中
+        insert_sql, params = item.get_insert_sql()
+        cursor.execute(insert_sql, params)
 
 
 class JsonExporterPipeline(object):
-	# 调用scrapy提供的json exporter导出json 文件
-	def __init__(self):
-		self.file = open('article.json', 'wb')
-		self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
-		self.exporter.start_exporting()
-	
-	def close_spider(self, spider):
-		self.exporter.finish_exporting()
-		self.file.close()
-	
-	def process_item(self, item, spider):
-		self.exporter.export_item(item)
-		return item
+    # 调用scrapy提供的json exporter导出json 文件
+    def __init__(self):
+        self.file = open('article.json', 'wb')
+        self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
+        return item
 
 
 class ArticleImagePipeline(ImagesPipeline):
-	def item_completed(self, results, item, info):
-		for ok, value in results:
-			image_file_path = value['path']
-		item['front_image_path'] = image_file_path
-		return item
+    def item_completed(self, results, item, info):
+        for ok, value in results:
+            image_file_path = value['path']
+        item['front_image_path'] = image_file_path
+        return item
