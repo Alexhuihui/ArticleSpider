7d1e63b65ab0e5b00e52e34c7c788e0858e1015d
alex <2930807240>
2019-01-10 12:25:30 +0800

    完成了jobbole的文章爬取

--- ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/spiders/jobbole.py
@@ -4,11 +4,10 @@ import datetime
 import scrapy
 import re
 from scrapy.http import Request
-from scrapy.loader import ItemLoader
 from urllib import parse
 from ArticleSpider.utils.common import get_md5
 
-from ArticleSpider.items import JobBoleArticleItem
+from ArticleSpider.items import JobBoleArticleItem, ArticleItemLoader
 
 
 class JobboleSpider(scrapy.Spider):
@@ -34,50 +33,61 @@ class JobboleSpider(scrapy.Spider):
 	
 	def parse_detail(self, response):
 		# 提取文章的具体信息
-		article_item = JobBoleArticleItem()
-		
-		title = response.css(".entry-header h1::text").extract_first("")
-		front_image_url = response.meta.get('front_image_url', '')
-		create_date = response.css("p.entry-meta-hide-on-mobile::text").extract_first("").strip().replace('·',
-		                                                                                                  '').strip()
-		praise_nums = response.css(".vote-post-up h10::text").extract_first("")
-		fav_nums = response.css(".bookmark-btn::text").extract_first("")
-		match_re = re.match(".*?(\d+).*", fav_nums)
-		if match_re:
-			fav_nums = match_re.group(1)
-		else:
-			fav_nums = 0
-		
-		comment_nums = response.css("a[href='#article-comment'] span::text").extract_first("")
-		match_re = re.match(".*?(\d+).*", comment_nums)
-		if match_re:
-			comment_nums = match_re.group(1)
-		else:
-			comment_nums = 0
-		
-		content = response.css("div.entry").extract_first("")
-		tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
-		tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
-		tags = ','.join(tag_list)
-		
-		article_item['url_object_id'] = get_md5(response.url)
-		article_item['title'] = title
-		article_item['url'] = response.url
-		try:
-			create_date = datetime.datetime.strtime(create_date, '%Y/%m/%d').date()
-		except Exception as e:
-			create_date = datetime.datetime.now().date()
-		article_item['create_date'] = create_date
-		article_item['front_image_url'] = [front_image_url]
-		article_item['praise_nums'] = praise_nums
-		article_item['comment_nums'] = comment_nums
-		article_item['fav_nums'] = fav_nums
-		article_item['tags'] = tags
-		article_item['content'] = content
-		
-		# 通过itmeLoader加载item
-		item_loader = ItemLoader(item=JobBoleArticleItem(), response=response)
-		item_loader.add_css()
+		# article_item = JobBoleArticleItem()
+		#
+		# title = response.css(".entry-header h1::text").extract_first("")
+		# front_image_url = response.meta.get('front_image_url', '')
+		# create_date = response.css("p.entry-meta-hide-on-mobile::text").extract_first("").strip().replace('·',
+		#                                                                                                   '').strip()
+		# praise_nums = response.css(".vote-post-up h10::text").extract_first("")
+		# fav_nums = response.css(".bookmark-btn::text").extract_first("")
+		# match_re = re.match(".*?(\d+).*", fav_nums)
+		# if match_re:
+		# 	fav_nums = match_re.group(1)
+		# else:
+		# 	fav_nums = 0
+		#
+		# comment_nums = response.css("a[href='#article-comment'] span::text").extract_first("")
+		# match_re = re.match(".*?(\d+).*", comment_nums)
+		# if match_re:
+		# 	comment_nums = match_re.group(1)
+		# else:
+		# 	comment_nums = 0
+		#
+		# content = response.css("div.entry").extract_first("")
+		# tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+		# tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+		# tags = ','.join(tag_list)
+		#
+		# article_item['url_object_id'] = get_md5(response.url)
+		# article_item['title'] = title
+		# article_item['url'] = response.url
+		# try:
+		# 	create_date = datetime.datetime.strtime(create_date, '%Y/%m/%d').date()
+		# except Exception as e:
+		# 	create_date = datetime.datetime.now().date()
+		# article_item['create_date'] = create_date
+		# article_item['front_image_url'] = [front_image_url]
+		# article_item['praise_nums'] = praise_nums
+		# article_item['comment_nums'] = comment_nums
+		# article_item['fav_nums'] = fav_nums
+		# article_item['tags'] = tags
+		# article_item['content'] = content
+
+		front_image_url = response.meta.get("front_image_url", "")  # 文章封面图
+		item_loader = ArticleItemLoader(item=JobBoleArticleItem(), response=response)
+		item_loader.add_css("title", ".entry-header h1::text")
+		item_loader.add_value("url", response.url)
+		item_loader.add_value("url_object_id", get_md5(response.url))
+		item_loader.add_css("create_date", "p.entry-meta-hide-on-mobile::text")
+		item_loader.add_value("front_image_url", [front_image_url])
+		item_loader.add_css("praise_nums", ".vote-post-up h10::text")
+		item_loader.add_css("comment_nums", "a[href='#article-comment'] span::text")
+		item_loader.add_css("fav_nums", ".bookmark-btn::text")
+		item_loader.add_css("tags", "p.entry-meta-hide-on-mobile a::text")
+		item_loader.add_css("content", "div.entry")
+
+		article_item = item_loader.load_item()
 		
 		# 跳转到piplines中
 		yield article_item
